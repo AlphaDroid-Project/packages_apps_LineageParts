6d29f66f8e13eff63a7a9629a5bdbeec360b9a54
Bruno Martins <bgcngm@gmail.com>
2025-12-03 21:27:39 +0000

    LineageParts: Inject LiveDisplay activity into Settings via IA (again)
    Respects the overlay config, but not longer requires adding an explicit
    link to the activity in Settings app.
    
    Note that LiveDisplay visibility must be toggled regardless if user is
    the owner or a secondary user.
    
    Change-Id: Ife11af2985bbb66ac474c526894b703ba0277ceb

--- src/com/android/settings/lineage/livedisplay/LiveDisplaySettings.java
+++ src/com/android/settings/lineage/livedisplay/LiveDisplaySettings.java
@@ -1,6 +1,6 @@
 /*
  * SPDX-FileCopyrightText: 2015 The CyanogenMod Project
- * SPDX-FileCopyrightText: 2017-2024 The LineageOS Project
+ * SPDX-FileCopyrightText: 2017-2025 The LineageOS Project
  * SPDX-License-Identifier: Apache-2.0
  */
 package com.android.settings.lineage.livedisplay;
@@ -47,6 +47,7 @@ import com.android.settings.lineage.SettingsPreferenceFragment;
 import com.android.settings.lineage.search.BaseSearchIndexProvider;
 import com.android.settings.lineage.search.SearchIndexableRaw;
 import com.android.settings.lineage.search.Searchable;
+import com.android.settings.lineage.utils.GenericUtils;
 import com.android.settings.lineage.utils.ResourceUtils;
 
 import java.util.Collections;
@@ -385,6 +386,11 @@ public class LiveDisplaySettings extends SettingsPreferenceFragment implements S
         updateReadingModeStatus();
     }
 
+    public static void restoreLiveDisplay(Context context) {
+        GenericUtils.setComponentEnabled(context, LiveDisplaySettings.class.getName(),
+                context.getResources().getBoolean(
+                        com.android.internal.R.bool.config_enableLiveDisplay));
+    }
 
     public static final Searchable.SearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
             new BaseSearchIndexProvider() {
